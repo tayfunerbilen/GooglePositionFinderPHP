298a531b8ca05e9f3e810ad77b9d5c08c7909abc
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-06-15 07:02:29 +0300

    Update example.php

--- example.php
+++ example.php
@@ -4,11 +4,11 @@ require_once 'GooglePositionFinder.class.php';
 
 $google = new GooglePositionFinder();
 
-$page = 2;
+$page = 1;
 $lang = 'com.tr'; // turkish
-$data = $google->query('facebook', $page, $lang);
+$data = $google->query('jquery dersleri', $page, $lang);
 
 print_r($data);
 
-$match = $google->matchResult('facebook.com');
+$match = $google->matchResult('erbilen.net');
 echo 'Aramalarda '.$match.' sonuçta siteniz yer alıyor.';
