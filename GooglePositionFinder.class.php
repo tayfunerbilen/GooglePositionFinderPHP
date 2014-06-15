8642ad811f72af0b74e69874fed354c63e1b4976
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-06-15 06:59:07 +0300

    Update GooglePositionFinder.class.php

--- GooglePositionFinder.class.php
+++ GooglePositionFinder.class.php
@@ -60,7 +60,7 @@ class GooglePositionFinder
 
         // $useragent = "Opera/9.80 (J2ME/MIDP; Opera Mini/4.2.14912/870; U; id) Presto/2.4.15";
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://www.google.' . $domain . '/search?q=' . rawurlencode($keyword) . '&start=' . $page);
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.' . $domain . '/search?q=' . rawurlencode($keyword) . '&start=' . $page);
         curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // set user agent
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
