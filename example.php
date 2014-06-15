0b9f8cb55cec1f5e40ac923c98d9326db4005ae0
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-06-15 05:12:02 +0300

    Update example.php

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ require_once 'GooglePositionFinder.class.php';
 $google = new GooglePositionFinder();
 
 $page = 2;
-$lang = 'tr'; // turkish
+$lang = 'com.tr'; // turkish
 $data = $google->query('facebook', $page, $lang);
 
 print_r($data);
