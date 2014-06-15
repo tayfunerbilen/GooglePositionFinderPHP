4287fb8e832cd0d3def3b8868723366d6d1c3a42
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-06-15 06:12:16 +0300

    Update example.php

--- example.php
+++ example.php
@@ -9,3 +9,6 @@ $lang = 'com.tr'; // turkish
 $data = $google->query('facebook', $page, $lang);
 
 print_r($data);
+
+$match = $google->matchResult('facebook.com');
+echo 'Aramalarda '.$match.' sonuçta siteniz yer alıyor.';
